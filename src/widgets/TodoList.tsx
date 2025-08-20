de784e3e1099a402982293e0162748fa21c5ef75
Rokcha_latte <ojr5521@naver.com>
2025-08-20 13:36:57 +0900

    ui 개조

--- src/widgets/TodoList.tsx
+++ src/widgets/TodoList.tsx
@@ -79,7 +79,6 @@ export default function TodoList() {
 
   useEffect(() => {
     if (hydrated && isAdmin) loadAll();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [hydrated, isAdmin]);
 
   const handleAdd = async () => {
